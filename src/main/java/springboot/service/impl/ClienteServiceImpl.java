b9a09fcf6e29d056200708756aa0ef0546889ff7
Iarlen <iarlensilva775@gmail.com>
2017-06-12 19:24:44 -0300

    Criando a implementação do Service Cliente

--- src/main/java/springboot/service/impl/ClienteServiceImpl.java
+++ src/main/java/springboot/service/impl/ClienteServiceImpl.java
@@ -1,5 +1,46 @@
 package springboot.service.impl;
 
-public class ClienteServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Cliente;
+import repository.ClienteRepository;
+import springboot.service.ClienteService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public void salvar(Cliente cliente) {
+
+	}
+
+	@Override
+	public void atualizar(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Cliente> getTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente buscarById(Integer id) {
+		return clienteRepository.findById(id);
+	} 
+
+	@Override
+	public void remove(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
