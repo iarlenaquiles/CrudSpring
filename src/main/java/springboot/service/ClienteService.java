ddf22b31100903a314827ff38c230cde3af683a2
Iarlen <iarlensilva775@gmail.com>
2017-06-12 19:17:25 -0300

    Criada a interface Service do Cliente

--- src/main/java/springboot/service/ClienteService.java
+++ src/main/java/springboot/service/ClienteService.java
@@ -1,8 +1,19 @@
 package springboot.service;
 
+import java.util.List;
+
 import model.Cliente;
 
 public interface ClienteService {
 
 	void salvar(Cliente cliente);
+
+	void atualizar(Cliente cliente);
+
+	List<Cliente> getTodos();
+
+	Cliente buscarById(Integer id);
+
+	void remove(Integer id);
+
 }
