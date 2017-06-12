2e75c9533caed38156c6731d0a207ee03b49fa71
Iarlen <iarlensilva775@gmail.com>
2017-06-12 19:31:56 -0300

    Atualizando o Service Cliente

--- src/main/java/springboot/service/ClienteService.java
+++ src/main/java/springboot/service/ClienteService.java
@@ -2,18 +2,19 @@ package springboot.service;
 
 import java.util.List;
 
+import exception.UniadvException;
 import model.Cliente;
 
 public interface ClienteService {
 
-	void salvar(Cliente cliente);
+	void salvar(Cliente cliente) throws UniadvException;
 
-	void atualizar(Cliente cliente);
+	void atualizar(Cliente cliente) throws UniadvException;
 
 	List<Cliente> getTodos();
 
 	Cliente buscarById(Integer id);
 
-	void remove(Integer id);
+	void remove(Integer id) throws UniadvException;
 
 }
